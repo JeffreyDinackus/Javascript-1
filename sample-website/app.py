f342257f737a56fecebe9b4452c1b1fd4b488a76
JeffreyDinackus <z3l9m@protonmail.com>
2023-02-05 17:07:47 -0500

    fixed javascript pathing, adding navbar and database

--- sample-website/app.py
+++ sample-website/app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+#the commented out stuff is code I would use if I want a database later
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///testdb.db'
+# db = SQLAlchemy(app)
+#
+#
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#this function tells SQL how to handle printing informatio when asked. 
+#     def __repr__(self):
+#         return 'self.id'
 
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+# @app.route('/about')
+# @app.route('/biography')
+# def index():
+#     return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
