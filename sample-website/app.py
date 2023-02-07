290a15cbc44df43e51408b495b4a6c3d6c8c5b31
JeffreyDinackus <z3l9m@protonmail.com>
2023-02-06 23:02:04 -0500

    adding masonry + some changes to base html

--- sample-website/app.py
+++ sample-website/app.py
@@ -19,10 +19,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/about')
-# @app.route('/biography')
-# def index():
-#     return render_template('about.html')
+@app.route('/about')
+@app.route('/biography')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
