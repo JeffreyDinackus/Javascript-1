32ee0eb15a8d0eb0933e509d72d57b62bc5f5a09
JeffreyDinackus <102326113+JeffreyDinackus@users.noreply.github.com>
2023-02-08 16:31:43 -0500

    Update app.py

--- sample-website/app.py
+++ sample-website/app.py
@@ -1,3 +1,4 @@
+#if you don't understand flask, use this video https://www.youtube.com/watch?v=Z1RJmh_OqeA. the first 15 minutes or so will help you explain flask without databases. 
 from flask import Flask, render_template
 #the commented out stuff is code I would use if I want a database later
 # from flask_sqlalchemy import SQLAlchemy
@@ -13,12 +14,14 @@ app = Flask(__name__)
 #     def __repr__(self):
 #         return 'self.id'
 
+#these are the routes I can reference to go to index.html. It is also the route route. It is referenced in the HTML as /home or / or /index, not by the file name.  
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+#these are the routes I can reference to go to about.html. 
 @app.route('/about')
 @app.route('/biography')
 def about():
