473afd48762578cc53054352be997b7b257997df
JeffreyDinackus <z3l9m@protonmail.com>
2023-02-20 19:54:47 -0500

    documentation added

--- midterm-website/app.py
+++ midterm-website/app.py
@@ -1,9 +1,10 @@
-#if you don't understand flask, use this video https://www.youtube.com/watch?v=Z1RJmh_OqeA. the first 15 minutes or so will help you explain flask without databases. 
+#if you don't understand flask, use this video https://www.youtube.com/watch?v=Z1RJmh_OqeA. the first 15 minutes or so will help you explain flask without databases. This is also a video I referenced when making this file.
 from flask import Flask, render_template, redirect
 #the commented out stuff is code I would use if I want a database later
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#ignore the database stuff unless I actually implement a database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///testdb.db'
 # db = SQLAlchemy(app)
 #
@@ -21,7 +22,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-#these are the routes I can reference to go to about.html. 
+#these are the routes, the first one is for a href to /about or /biography. when you reference the app.route, it will serve a function which renders a template(the html in the templates folder) 
 @app.route('/about')
 @app.route('/biography')
 def about():
@@ -34,7 +35,7 @@ def youtube():
 @app.route('/registration')
 def registration():
     return render_template('registration.html')
-#these are for the dummy social media buttons, they are simply show I know how to redirect to a social media page (I have none for this website)
+#these are for the dummy social media buttons, they are simply show I know how to redirect to a social media page (I have no social media for this website)
 
 @app.route('/externalyoutube')
 def externalyoutube():
@@ -52,5 +53,6 @@ def externaltwitter():
 def externaltelegram():
     return redirect("http://www.telegram.org")
 
+#debug=true means it will change on refresh, this is not to be used for production level websites. if __name__ == __main__ is a check to make sure it is working properly
 if __name__ == "__main__":
     app.run(debug=True)
