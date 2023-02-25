409636c5e1f34bd3a90a0659d6c594bed341f279
JeffreyDinackus <102326113+JeffreyDinackus@users.noreply.github.com>
2023-02-25 16:16:35 -0500

    Add files via upload

--- in-class-assignments/2-18-Website/scripts/app.js
+++ in-class-assignments/2-18-Website/scripts/app.js
@@ -7,3 +7,18 @@ console.log("Ahoy World!");
 console.info("Don't chew my bubblegum!");
 
 alert("don't eat my bullchew!");
+
+console.log("don't screw the pooch!");
+
+let message = "it's almost time to go home.";
+
+alert(message);
+
+let message2 = "JK you'll be here forever";
+
+alert(message2);
+
+let isGoing = false;
+isGoing = confirm("are you going to prom?");
+//confirm is a alert that takes a response
+alert("Going to prom " + isGoing);
