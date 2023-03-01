07bd0926a32464af2afacf3ca23d58f220d8544b
JeffreyDinackus <z3l9m@protonmail.com>
2023-03-01 14:43:58 -0500

    more work added

--- homework/assignment-1/scripts/app.js
+++ homework/assignment-1/scripts/app.js
@@ -26,10 +26,44 @@ alert(`Your string in reverse is: ${userName3.split("").reverse().join("")}`);
 let b = prompt("enter a side for a");
 let a = prompt("enter a side for b");
 let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-
+//it says to make it a log in the console for this problem.
 console.log(c);
 
 //this is problem 2.4
-let y = 2;
 
-//this is the chapter 2 project, Mils to Kilometers.
+// Add b to a
+// Divide a by c
+// Replace the value of c with the modulus of c and b
+// Print all three numbers to the console
+a = +a + +b; //the way they wrote this is sorta unclear. I interpreted what they wrote as adding b to a.
+//It will concatenate without the + in front of the number.
+a = +a / +c;
+c = +c % +b;
+alert(`a == ${a}, b == ${b}, c == ${c}`);
+
+//This is the first chapter 2 project, miles to kilometers.
+
+let miles = +10;
+let kilometers = +miles * +1.60934;
+
+console.log(`The distance of ${kilometers} kms is equal to ${miles} miles`);
+//the original text from the textbook is, "The distance of 130 kms is equal to 209.2142 miles" This doesn't make sense because a kilometer is shorter than a mile. I think it is a error in the textbook. 
+
+//This is the second chapter 2 project, BMI.
+
+let inches = 72; //6 feet
+let pounds = 150;
+
+let centimeters = +inches * +2.54;
+let kilos = +pounds / +2.2046;
+let meters = centimeters / 100;
+
+//outputs variables to console. Mostly for my own checking
+console.log(
+  `Inches: ${inches}, Pounds: ${pounds},  Centimeters: ${centimeters}, Kilos: ${kilos}`
+);
+
+let BMI = +kilos / (+meters * +meters);
+
+console.log(`BMI is ${BMI}`);
+
