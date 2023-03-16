8b63e90aeb99968e341cefae72175b2f53cccdb3
JeffreyDinackus <z3l9m@protonmail.com>
2023-03-15 23:00:24 -0400

    Made

---
+++ in-class-assignments/2-18-Website/scripts/app2.js
@@ -0,0 +1,17 @@
+let classone = "ITP 270: Programming for Cybersecurity";
+let classtwo = "ITP 120: Java 1";
+let classthree = "ITN 200: Networking Administration";
+
+
+
+function vowels {
+
+}
+
+function consenants {
+
+}
+
+
+
+console.log(`There are ${vowels} vowels and ${consenants} consenants in these courses.`);
