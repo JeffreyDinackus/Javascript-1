9469995e17fc4ffb4d361373f0cbc34599d22632
JeffreyDinackus <z3l9m@protonmail.com>
2023-04-08 15:55:08 -0400

    updates to classwork

--- in-class-assignments/DOM/dom.js
+++ in-class-assignments/DOM/dom.js
@@ -1,4 +1,4 @@
-const TEXTCONTENT = document.querySelector("#test").textContent;
+const TEXTCONTENT = document.querySelector("#test");
 const ELEMENTINNERNODE = document.querySelector("#test").innerHTML;
 const ELEMENTOUTERNODE = document.querySelector("#test").outerHTML;
 const TEXTELEM = document.querySelector("#test").innerText;
@@ -7,3 +7,6 @@ console.log(`this is the text content ${TEXTCONTENT}`);
 console.log(`this is the inner html ${ELEMENTINNERNODE}`);
 console.log(`This is the outer html ${ELEMENTOUTERNODE}`);
 console.log(`This is the inner text ${TEXTELEM}`);
+
+TEXTCONTENT.textContent = "hello sunshine";
+console.log(`this is the text content ${TEXTCONTENT.textContent}`);
