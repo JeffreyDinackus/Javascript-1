ba0bdb9c39017185e77ba7bc0ee84797bf136132
JeffreyDinackus <z3l9m@protonmail.com>
2023-04-10 13:48:22 -0400

    extra credit done

--- midterm-website/static/homework/scripts/extracredit1.js
+++ midterm-website/static/homework/scripts/extracredit1.js
@@ -1,6 +1,7 @@
 function generatePasswd() {
   var length = 8,
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=",
     retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
     retVal += charset.charAt(Math.floor(Math.random() * n));
@@ -9,16 +10,20 @@ function generatePasswd() {
 }
 
 function customPasswd() {
-  length = prompt("How long do you want your password to be?");
+  length = prompt(
+    "How long do you want your password to be? type in number form, the length in characters of the password."
+  );
   includeNumbers = prompt(
-    "Do you want numbers in your password? enter 'false' exactly if not or 'true' exactly if yes."
+    "Do you want numbers in your password? enter 'true' exactly if yes or 'false' exactly if not."
   );
   includeSymbols = prompt(
     "Do you want symbols in your password? enter 'true' exactly if yes or 'false' exactly if no."
   );
   if (includeSymbols == "true" && includeNumbers == "true") {
+    //this is the chacter set used to make the password
     (charset =
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-="),
+      //this is the value that will store the password
       (retVal = "");
   } else if (includeNumbers == "false" && includeSymbols == "false") {
     (charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
@@ -26,6 +31,10 @@ function customPasswd() {
   } else if (includeNumbers == "true" && includeSymbols == "false") {
     (charset =
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"),
+      (retVal = "");
+  } else if (includeNumbers == "false" && includeSymbols == "true") {
+    (charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+~`|}{[]:;?><,./-="),
       (retVal = "");
   } else {
     (charset =
