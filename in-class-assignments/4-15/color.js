83e19ecfdf9e7c9434c72612b3f58b6c03c3e6aa
JeffreyDinackus <JeffreyDinackus@proton.me>
2023-04-15 16:18:11 -0400

    color changer made during class

---
+++ in-class-assignments/4-15/color.js
@@ -0,0 +1,6 @@
+let body = document.querySelector('#changeMe');
+let button = document.querySelector('#changeColor');
+
+button.addEventListener('click', function () {
+  body.style.backgroundColor = 'red';
+});
