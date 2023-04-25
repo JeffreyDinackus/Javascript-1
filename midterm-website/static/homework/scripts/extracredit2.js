a9be71b6867959fea0dc373ea1b910b9ad4a44cd
JeffreyDinackus <JeffreyDinackus@proton.me>
2023-04-25 15:05:34 -0400

    new comments explaining the program

--- midterm-website/static/homework/scripts/extracredit2.js
+++ midterm-website/static/homework/scripts/extracredit2.js
@@ -3,6 +3,16 @@ let slackNotif = false;
 let emailNotif = false;
 let instagramNotif = false;
 
+//explains the business problem and requirements
+
+//the business problem is that the user wants to be able to recieve notifications from multiple channels at once. One might say, if my house was burning down, I'd like to recieve all the notifications I can. However, if we were to make code that gathers the user wants notifications from all channels, we would have to make a lot of if statements. This is not good code.
+//We want to make code that is easy to read and easy to maintain. So, we will use the observer pattern to solve this problem. The observer pattern is a design pattern that allows us to notify multiple objects of a change in state. In this case, we will notify the user of a change in state of the notifications. The user will be able to subscribe to the notifications they want to recieve.
+//The user will be able to unsubscribe from the notifications they don't want to recieve. The user will be able to recieve notifications from the notifications they subscribed to. The main purpose is that this solution doesn't bloat the code with if statements and makes it easy to maintain.
+
+//discusses the patterns which could have been applied
+
+//I don't know more than a few patterns so this is the only pattern that would work for the problem at hand. I certainly like this method of solving this particular problem. This is how I would probably solve this problem given the choice.
+
 function addFacebookNotifications() {
   facebookNotif = true;
 }
