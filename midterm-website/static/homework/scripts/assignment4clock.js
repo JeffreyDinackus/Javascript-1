f168e23d928d3fd2ad928ee5dd1f535e351e0ed6
JeffreyDinackus <jeffreydinackus@proton.me>
2023-05-01 15:18:00 -0400

    more validation errors fixed

--- midterm-website/static/homework/scripts/assignment4clock.js
+++ midterm-website/static/homework/scripts/assignment4clock.js
@@ -33,3 +33,40 @@ function twelveHourClock() {
   document.getElementById('clock2').style.font = 'italic bold 20px arial,serif';
 }
 
+  
+    // function digitalClock() {
+    //   var date = new Date();
+    //   var hours = date.getHours();
+    //   var minutes = date.getMinutes();
+    //   var seconds = date.getSeconds();
+    //   var time = hours + ':' + minutes + ':' + seconds;
+    //   document.getElementById('clock').innerText = time;
+    //   setTimeout(digitalClock, 1000);
+    //   document.getElementById('clock').style.font =
+    //     'italic bold 20px arial,serif';
+    // }
+
+    // function twelveHourClock() {
+    //   //get the date object
+    //   var date = new Date();
+    //   //get the properties of the date object used for the clock
+    //   var hours = date.getHours();
+    //   var minutes = date.getMinutes();
+    //   var seconds = date.getSeconds();
+    //   //checks if the time is AM or PM and sets accordingly
+    //   var ampm = hours >= 12 ? 'pm' : 'am';
+    //   //turns a 24 hour clock into a 12 hour clock
+    //   hours = hours % 12;
+    //   // the hour '0' should be '12'
+    //   hours = hours ? hours : 12;
+    //   minutes = minutes < 10 ? '0' + minutes : minutes;
+    //   //formats the string properly
+    //   var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+    //   //sets the text of the clock to the formatted string
+    //   document.getElementById('clock2').innerText = strTime;
+    //   //sets the clock to update every second
+    //   setTimeout(twelveHourClock, 1000);
+    //   //sets the font of the clock
+    //   document.getElementById('clock2').style.font =
+    //     'italic bold 20px arial,serif';
+    // }
