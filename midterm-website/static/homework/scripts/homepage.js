30ba1224d56db9c11606b52f3211f117854cb4d8
JeffreyDinackus <jeffreydinackus@proton.me>
2023-05-09 23:30:16 -0400

    new comments

--- midterm-website/static/homework/scripts/homepage.js
+++ midterm-website/static/homework/scripts/homepage.js
@@ -2,9 +2,11 @@ const urlParams = new URLSearchParams(window.location.search);
 const email = urlParams.get("email");
 const passwd = urlParams.get("passwd");
 
+//This only works with the single login for the admin. 
+
 const loggedinas = document.getElementById("loggedinas");
 if (email && passwd) {
   loggedinas.innerHTML = `Welcome Admin, your email is: ${email}!`;
 } else {
   loggedinas.textContent = "Welcome to the website";
-}
+} 
